d36df286a7a18e12fc9502225dab28969fed1a94
Daner SHARIFI <64554402+DanerSharifi-FR@users.noreply.github.com>
2023-11-20 09:45:30 +0100

    Update Daner_SHARIFI.java

--- s1.01/Daner_SHARIFI.java
+++ s1.01/Daner_SHARIFI.java
@@ -3,191 +3,42 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Sae101_4A4 {
-    /**
-     * -->Développé par Romain Da Chao<--
-     * <p>
-     * Méthode principale du programme permettant de gérer la competition
-     * en saisissant le nombre de compétiteurs, la longueur de la piste, le nombre d'obstacles (avec un maximum en cohérence avec la piste)
-     * et le nombre total de barres.
-     *
-     * @param args Les paramètres de la fonction main (qui sont inutilisés dans ce cas)
-     */
     public static void main(String[] args) {
+        int[] values1 = {9, 0, 44, 5, -5, 9, 120, 3, -1, 9, 0, 88, 9};
+        System.out.println("int[] compétiteurs = {9, 0, 44, 5, -5, 9, 120, 3, -1, 9, 0, 88, 9}");
+        System.out.println("testCas1() :\n");
+        affichageTousConcurrentsDansOrdreBrassards(values1);
+        podium(values1);
+        System.out.println("-------------------------\n");
 
-        System.out.println("Saisissez le nombre de competiteurs :");
-        int nbCompetiteurs = saisieIntC(1, 50); //Saisie des compétiteurs en appelant la fonction de saisie contrôlée saisieIntC
+        System.out.println("int[] compétiteurs = {9, 0, 44, 5, 120, 3, 0, 88}");
+        int[] values2 = {9, 0, 44, 5, 120, 3, 0, 88};
+        System.out.println("testCas2() :\n");
+        affichageTousConcurrentsDansOrdreBrassards(values2);
+        podium(values2);
+        System.out.println("-------------------------\n");
 
-        int[] temps = new int[nbCompetiteurs]; //Création de la liste pour insérer les temps réalisés par les joueurs
-        SaisieJoueurs(nbCompetiteurs, temps); //Attribution des numéros de joueurs dans la liste joueurs avec l'appel de la fonction SaisieJoueurs
+        System.out.println("int[] compétiteurs = {9, 0, 3, 5, 120, 3, 0, 88}");
+        int[] values3 = {9, 0, 3, 5, 120, 3, 0, 88};
+        System.out.println("testCas3() :\n");
+        affichageTousConcurrentsDansOrdreBrassards(values3);
+        podium(values3);
+        System.out.println("-------------------------\n");
 
-        System.out.println("Saisissez la longueur de la piste (en mètres) :"); //Saisie de la longueur de la piste
-        int lgPiste = saisieIntC(400, 800); //Saisie de la longueur de la piste en appelant la fonction de saisie contrôlée saisieIntC
+        System.out.println("int[] compétiteurs = {9, 0, 75, 5, 120, 3, 0, 88, 5}");
+        int[] values4 = {9, 0, 75, 5, 120, 3, 0, 88, 5};
+        System.out.println("testCas4() :\n");
+        affichageTousConcurrentsDansOrdreBrassards(values4);
+        podium(values4);
+        System.out.println("-------------------------\n");
 
-        System.out.println("Saisissez le nombre d'obstacle");
-        int nbObstacles = saisieIntC(0, 25); //Saisie du nombre d'obstacles de la piste en appelant la fonction de saisie contrôlée saisieIntC
+        System.out.println("int[] compétiteurs = {9, 0, 3, 5, 120, 3, 0, 88, 3, 928, 8, 3, 11, 0, 3}");
+        int[] values5 = {9, 0, 3, 5, 120, 3, 0, 88, 3, 928, 8, 3, 11, 0, 3};
+        System.out.println("testCas5() :\n");
+        affichageTousConcurrentsDansOrdreBrassards(values5);
+        podium(values5);
+        System.out.println("-------------------------\n");
 
-        System.out.println("Saisissez le nombre total de barres");
-        int nbTotalesBarres = saisieIntC(nbObstacles * 2, nbObstacles * 4); //Saisie du total des barres en veillant sur la cohérence avec le nombre d'obstacles avec saisieIntC
-
-        System.out.println("Le nombre de competiteurs est de " + nbCompetiteurs + " personnes.\n" +
-                "La longueur de la piste est de " + lgPiste + " mètres.\n" +
-                "Le nombre d'obstacle est de " + nbObstacles + " obstacles.\n" +
-                "Le nombre totale de barres est de " + nbTotalesBarres + " barres. " +
-                "Soit en moyenne " + (int) nbTotalesBarres / nbObstacles + " barres par obstacles."); //Récapitulatif des données attribuées
-
-        //resultats(nbCompetiteurs, nbTotalesBarres, nbObstacles, lgPiste, temps); //Appel de la fonction resultats pour retourner la liste de numéro de joueurs et leur temps attribué
-        int[] competiteursTableau = resultats(nbCompetiteurs, nbTotalesBarres, nbObstacles, lgPiste, temps);
-        affichageTousConcurrentsDansOrdreBrassards(competiteursTableau);
-        podium(competiteursTableau);
-        System.out.println("------------------------------");
-        System.out.println("Place à la manche 2 :");
-        int[] competiteursTableau2 = manche2(nbCompetiteurs, nbTotalesBarres, nbObstacles, lgPiste, temps);
-        affichageTousConcurrentsDansOrdreBrassards(competiteursTableau2);
-        podium(competiteursTableau2);
-    }
-
-    /**
-     * Demande à l'utilisateur de saisir un entier, jusqu'à ce que
-     * l'entier saisi soit entre les deux bornes en paramètres.
-     *
-     * @param pfBorneInf IN : borne inférieure
-     * @param pfBorneSup IN : borne supérieure
-     * @return un entier entre pfBorneInf et pfBorneSup, compris
-     */
-    public static int saisieIntC(int pfBorneInf, int pfBorneSup) {
-        int valeur;
-        Scanner clavier = new Scanner(System.in);
-        System.out.println("(Avec une valeur comprise entre " + pfBorneInf + " et " + pfBorneSup + ") :");
-        valeur = clavier.nextInt();
-        while (valeur < pfBorneInf || valeur > pfBorneSup) {
-            System.out.println("Attention ! Donnez une valeur comprise entre " + pfBorneInf + " et " + pfBorneSup + " !");
-            valeur = clavier.nextInt();
-        }
-        return valeur;
-    }
-
-    /**
-     * -->Développé par Romain Da Chao<--
-     *
-     * Saisie des numéro de brassard des joueurs et stockage dans le tableau.
-     *
-     * @param pfNbCompetiteurs IN : Le nombre de compétiteurs.
-     * @param pfTabJoueurs     IN : Le tableau de joueurs.
-     */
-    public static void SaisieJoueurs(int pfNbCompetiteurs, int pfTabJoueurs[]) {
-        for (int i = 0; i < pfNbCompetiteurs; i++) { //boucle for permettant d'entrer les numéros de joueurs dans pfTabJoueurs (pour mieux                                                         se repérer dans le code)
-            pfTabJoueurs[i] = i + 1; //Attribution des numéros pour chaques occurences.
-        }
-    }
-
-    /**
-     * @author Marwane Ibrahim
-     *
-     * Permet de saisir le nombre de barres tombées, le nombre de refus, s'il y a eu une chute, son temps en millisecondes et
-     * de dire si le candidat de numéro pfBrassard est qualifié ou non.
-     *
-     * @param pfBrassard        IN : numéro de brassard du compétiteur
-     * @param pfNbTotalesBarres IN : nombre totales de barres
-     * @param pfNbObstacles     IN : nombre d'obstacles
-     * @param pfLgPiste         IN : longueur de la piste en mètre
-     * @return : le temps compensé du compétiteur s'il est qualifié 0 sinon
-     */
-    public static int manche(int pfBrassard, int pfNbTotalesBarres, int pfNbObstacles, int pfLgPiste) {
-        // Saisies
-        // Saisie contrôlée du nombre de barres tombées
-        Scanner clavier = new Scanner(System.in);
-        System.out.println("Saisissez le nombre de barres tombées par le compétiteur numéro " + pfBrassard + ": ");
-        int nbBarresT = saisieIntC(0, pfNbTotalesBarres);
-        while (nbBarresT > pfNbTotalesBarres) {
-            System.out.println("Erreur ! Donnez une valeur comprise entre " + pfNbTotalesBarres / 2 + " et " + pfNbTotalesBarres + " ?");
-            pfNbTotalesBarres = clavier.nextInt();
-        }
-        // Saisie contrôlée du nombre de refus
-        System.out.println("Saisissez le nombre de refus effectués par le cheval du compétiteur numéro " + pfBrassard + ": ");
-        int nbRefus = saisieIntC(0, pfNbObstacles);
-
-        // Saisie de la chute ou non
-        System.out.println("Saisissez si le compétiteur numéro " + pfBrassard + " a fait une chute: ");
-        System.out.println("Saisissez en minuscule oui si il a fait une chute et non sinon : ");
-        String chuteS = clavier.nextLine();
-
-        boolean chute = false;
-        // Saisie du temps effectué par le compétiteur
-        System.out.println("Saisissez le temps en millisecondes : ");
-        int temps = saisieIntC(0, 360000);
-        // Contrôle de si le compétiteur a chuté ou non
-        if (chuteS.equals("oui")) {
-            chute = true;
-        }
-
-        //Traitement
-        // Contrôle des conditions d'éliminations
-        int tempsCompense;
-        if (
-                (nbRefus >= 3)
-                        ||
-                        (pfLgPiste <= 600 && temps >= 120000)
-                        ||
-                        (pfLgPiste >= 600 && temps >= 180000)
-                        ||
-                        (chute == true)
-
-        ) {
-            tempsCompense = 0;
-        } else {
-            // Calcul du temps compensé
-            tempsCompense = 8000 * nbBarresT;
-            tempsCompense += temps;
-        }
-        // Return de la valeur finale
-        return tempsCompense;
-    }
-
-    /**
-     * -->Développé par Romain Da Chao<--
-     * <p>
-     * Saisie du nombre de competiteurs,du numéro de brassard, du nombre de barres, du nombre d'obstacles et longueur de la piste.
-     *
-     * @param pfNbObstacles     IN : nombre d'obstacles
-     * @param pfLgPiste         IN : longueur de la piste en mètre
-     * @param pfNbCompetiteurs  IN : Le nombre de compétiteurs
-     * @param pfNbTotalesBarres IN : Le nombre total de barres
-     * @param pfTabTemps        IN : Le tableau du temps des joueurs
-     * @return La liste du temps attribué à chaque joueur
-     */
-    public static int[] resultats(int pfNbCompetiteurs, int pfNbTotalesBarres, int pfNbObstacles, int pfLgPiste, int[] pfTabTemps) {
-
-        for (int i = 0; i < pfNbCompetiteurs; i++) { //boucle for pour insérer les joueurs et leurs temps dans resultats
-            pfTabTemps[i] = manche(i + 1, pfNbTotalesBarres, pfNbObstacles, pfLgPiste); //Attribution du temps du joueur au lieu du numéro
-        }
-        return pfTabTemps;
-    }
-
-    /**
-     * @author Romain Da Chao et Marwane Ibrahim
-     * 
-     * @param pfNbCompetiteurs IN : Le nombre de compétiteurs
-     * @param pfNbTotalesBarres IN : Le nombre total de barres
-     * @param pfNbObstacles IN : nombre d'obstacles
-     * @param pfLgPiste IN : longueur de la piste en mètre
-     * @param pfTabTemps IN : Le tableau du temps des joueurs
-     * 
-     * @return la liste de la moyenne des temps de la première et la deuxième manche
-     */
-    public static int[] manche2(int pfNbCompetiteurs, int pfNbTotalesBarres, int pfNbObstacles, int pfLgPiste, int[] pfTabTemps) {
-        // Remplissage du tableau tabTemps2[] par les résultats de la première manche
-        int tabTemps2[] = new int[pfNbCompetiteurs];
-        for (int i = 0; i < pfNbCompetiteurs; i++) {
-            tabTemps2[i] = pfTabTemps[i];
-        }
-        // Vérification du temps des compétiteurs, si la valeur vaut 0 il n'est pas qualifié et on ne s'occupe plus de la valeure, si il est qualifié on calcule la moyenne du temps de la première et de la deuxième manche.
-        for (int i = 0; i < tabTemps2.length; i++) {
-            if (tabTemps2[i] != 0) {
-                tabTemps2[i] += manche(i + 1, pfNbTotalesBarres, pfNbObstacles, pfLgPiste);
-                tabTemps2[i] = tabTemps2[i]/2;
-            }
-        }
-        return tabTemps2;
     }
 
     /**
